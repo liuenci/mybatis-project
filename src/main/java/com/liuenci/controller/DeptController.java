352642a30cc6fdf5b18d0eb95d6e053e6dec6a8c
liuenci <154910381@qq.com>
2018-05-10 21:09:17 +0800

    mybatis 连接 数据库

---
+++ src/main/java/com/liuenci/controller/DeptController.java
@@ -0,0 +1,40 @@
+package com.liuenci.controller;
+
+import com.liuenci.pojo.Dept;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+
+import java.io.IOException;
+import java.io.Reader;
+import java.util.List;
+
+public class DeptController {
+
+    static SqlSession session;
+    @BeforeClass
+    public static void init(){
+        try {
+            // 1. 加载配置
+            Reader config = Resources.getResourceAsReader("mybatis-config.xml");
+            // 2. 创建 Session 工厂
+            SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(config);
+            // 3. 开启 Session 得到 Session 对象
+            session = sqlSessionFactory.openSession();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    @Test
+    public void test1(){
+        // 4. 通过 Session 调用指定配置的 Sql 语句
+        List<Dept> list = session.selectList("getAllDept");
+        for (Dept dept: list){
+            System.out.println(dept);
+        }
+    }
+}
---
+++ src/main/java/com/liuenci/dao/DeptMapper.java
@@ -0,0 +1,10 @@
+package com.liuenci.dao;
+
+import com.liuenci.pojo.Dept;
+
+import java.util.List;
+public interface DeptMapper {
+
+    List<Dept> getAllDept();
+
+}
